7f4507ce3f6971c398e9949f4e8fd85abe9a3e78
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2026-02-14 02:42:40 +0200

    Add inventory management
    Player can now get, drop and use items from the inventory

--- src/Entities/entity.gd
+++ src/Entities/entity.gd
@@ -19,6 +19,7 @@ var grid_position: Vector2i:
 var fighter_component: FighterComponent
 var ai_component: BaseAIComponent
 var consumable_component : ConsumableComponent
+var inventory_component: InventoryComponent
 
 func _init(map_data: MapData, start_position: Vector2i, entity_definition: EntityDefinition) -> void:
 	centered = false
@@ -52,6 +53,10 @@ func set_entity_type(entity_definition: EntityDefinition) -> void:
 		if entity_definition.consumable_definition is HealingConsumableComponentDefinition:
 			consumable_component = HealingConsumableComponent.new(entity_definition.consumable_definition)
 			add_child(consumable_component)
+			
+	if entity_definition.inventory_capacity > 0:
+		inventory_component = InventoryComponent.new(entity_definition.inventory_capacity)
+		add_child(inventory_component)
 
 func is_blocking_movement() -> bool:
 	return blocks_movement
